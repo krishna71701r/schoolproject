c5c27edcdf98d0fd6bee4e344a2e15343cb98dfe
Krishna Kumar <akkrishnayadav71701r@gmail.com>
2024-08-30 16:07:58 +0530

    add display genre feature and admin dashboard feature with  all models

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,9 +2,33 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra=0
+class BookInline(admin.TabularInline):
+    model=Book
+    extra=0
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines=[BookInline,]
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author','language','display_genre')
+    inlines=[BooksInstanceInline,]
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display =('id','status', 'due_back','imprint')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
-admin.site.register(Book)
-admin.site.register(Author)
+admin.site.register(Book,BookAdmin)
+admin.site.register(Author,AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance,BookInstanceAdmin)
 admin.site.register(Language)
